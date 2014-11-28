854bf129529409c1e48b7df8f69d23f5aaf07611
Alexander Emelyanov <alexander.emelyanov@faunusanalytics.com>
2014-11-29 01:27:30 +0300

    Small interface update

--- views/default/index.php
+++ views/default/index.php
@@ -16,11 +16,9 @@ use yii\helpers\Html;
 <div class="i18n-default-index">
     <h1><?php echo Html::encode($this->title) ?></h1>
     <p>
-        <?= Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => 'Translation']), ['translations/'], ['class' => 'btn btn-success']) ?>
-    </p>
-    <hr>
-    <p>
-        <?php echo Html::a(Yii::t('app', 'Messages'), ['messages/'], ['class' => 'btn btn-success']) ?>
-        <?php echo Html::a(Yii::t('app', 'Source messages'), ['source-messages/'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', '{modelClass}', ['modelClass' => 'Translations']), ['translations/'], ['class' => 'btn btn-success']) ?>
+        <?= Html::a(Yii::t('app', 'Create {modelClass}', ['modelClass' => 'Translation']), ['translations/create'], ['class' => 'btn btn-primary']) ?>
+        <?= Html::a(Yii::t('app', 'Messages'), ['messages/'], ['class' => 'btn btn-default']) ?>
+        <?= Html::a(Yii::t('app', 'Source messages'), ['source-messages/'], ['class' => 'btn btn-default']) ?>
     </p>
 </div>
